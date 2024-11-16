55fc823289d5f4a95b918bf6acd114e0fb3fd3ee
Agustin-Ibarra <agustingsoc@gmail.com>
2024-11-16 20:11:02 -0300

    implementar funcionalidad para la ruta /register

--- Back/src/app.js
+++ Back/src/app.js
@@ -3,12 +3,13 @@ const cors = require("cors");
 const middleware = require("./middleware/middleware");
 const app = express();
 const { connectToDatabase } = require("./db/database");
+const router = require("./routes/routes");
 
 connectToDatabase();
 
 app.use(express.json());
 app.use(cors());
-
+app.use(router);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
---
+++ Back/src/controllers/user.controller.js
@@ -0,0 +1,23 @@
+const bcrypt = require("bcrypt");
+const User = require("../models/User.js");
+
+const register = async function(req,res){
+  const name = req.body.name;
+  const lastname = req.body.lastname;
+  const email = req.body.email;
+  const password = req.body.password;
+  const gender = req.body.gender;
+  const phone = req.body.phone;
+  const salt = await bcrypt.genSalt(5);
+  const hash = await bcrypt.hash(password,salt);
+  console.log(name,lastname,email,hash,gender,phone);
+  User.create({name:name,lastName:lastname,email:email,password:hash,phone:phone,gender:gender,role:"patient"})
+  .then((result)=>{
+    res.send('success');
+  })
+  .catch((error)=>{
+    console.log(error);
+  });
+}
+
+module.exports = {register};
