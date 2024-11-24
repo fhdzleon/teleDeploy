6c366cc427e3b35589fc1cf6fee7e877ad6a954c
fhdzleon <fhdzleon@gmail.com>
2024-11-23 21:20:54 -0600

    pathroutes y vista inn

---
+++ Front/telemed/src/app/in/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Page = () => {
+  return (
+    <div>
+      <h2>Bienvenido &apos;Usuario&apos;</h2>
+      <div>
+        <button disabled>Mis datos</button>
+        <button disabled>Mis Turnos</button>
+        <button disabled>Cerrar sesión</button>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
