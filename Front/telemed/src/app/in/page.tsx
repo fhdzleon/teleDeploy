fd43b5e0ee2194e258eedeb54a31b71085d5c7c0
fhdzleon <fhdzleon@gmail.com>
2024-11-23 21:33:54 -0600

    first commit in view

--- Front/telemed/src/app/in/page.tsx
+++ Front/telemed/src/app/in/page.tsx
@@ -2,12 +2,18 @@ import React from "react";
 
 const Page = () => {
   return (
-    <div>
+    <div className="flex flex-col justify-center items-center">
       <h2>Bienvenido &apos;Usuario&apos;</h2>
-      <div>
-        <button disabled>Mis datos</button>
-        <button disabled>Mis Turnos</button>
-        <button disabled>Cerrar sesión</button>
+      <div className="flex flex-col space-y-4">
+        <button className="rounded-full text-white purple px-4 py-2" disabled>
+          Mis datos
+        </button>
+        <button className="rounded-full text-white purple px-4 py-2" disabled>
+          Mis Turnos
+        </button>
+        <button className="rounded-full text-white purple px-4 py-2" disabled>
+          Cerrar sesión
+        </button>
       </div>
     </div>
   );
