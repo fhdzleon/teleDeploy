f7e9b524be9733bf673d863b9a8fdc7ea3edb014
Dmitri Jrapach <dmitrijrapach@gmail.com>
2024-11-27 21:09:30 -0300

    acomodando rutas

--- Back/src/routes/admin.js
+++ Back/src/routes/admin.js
@@ -1,17 +1,17 @@
 
-const express = require('express');
-const AdminController = require('../controllers/adminController');
-const roleAuthorization = require("../middleware/roleAuthorization.js");
+// const express = require('express');
+// const AdminController = require('../controllers/adminController');
+// const roleAuthorization = require("../middleware/roleAuthorization.js");
 
-const router = express.Router();
+// const router = express.Router();
 
-router.post('/turnos', roleAuthorization(["admin"]), AdminController.crearTurno);
+// router.post('/turnos', roleAuthorization(["admin"]), AdminController.crearTurno);
 
-router.put('/turnos/:turnoId', roleAuthorization(["admin"]), AdminController.editarTurno);
+// router.put('/turnos/:turnoId', roleAuthorization(["admin"]), AdminController.editarTurno);
 
-router.delete('/turnos/:turnoId', roleAuthorization(["admin"]), AdminController.eliminarTurno);
+// router.delete('/turnos/:turnoId', roleAuthorization(["admin"]), AdminController.eliminarTurno);
 
-// Ver turnos publicados (para pacientes)
-router.get('/turnos', AdminController.verTurnos);
+// // Ver turnos publicados (para pacientes)
+// router.get('/turnos', AdminController.verTurnos);
 
-module.exports = router;
+// module.exports = router;
--- Back/src/routes/routes.js
+++ Back/src/routes/routes.js
@@ -4,7 +4,7 @@ const { checkRegister, checkLogin } = require("../middleware/middleware");
 const { getDiagnosis, createDiagnosis } = require("../controllers/doctor.controller");
 const { addHealthcareSystem } = require("../controllers/healthcareSystemController");
 const { reserveTurn } = require("../controllers/turnController.js");
-
+const AdminController = require('../controllers/adminController');
 
 const router = express.Router();
 
@@ -13,9 +13,13 @@ router.use('/login/api',checkLogin);
 
 router.post('/register/api',register);
 router.post('/login/api',login);
+
+
+router.put("/reserve-turn", reserveTurn);
+router.get('/turnos', AdminController.verTurnos);
+
+router.post("/add", addHealthcareSystem);
 router.get('/diagnosis/:id',getDiagnosis);
 router.post("/create",createDiagnosis);
-router.post("/add", addHealthcareSystem);
-router.put("/reserve-turn", reserveTurn);
 
 module.exports = router;
