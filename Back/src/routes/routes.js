71a8fc348d262c9de18961be0f9bcc619dae2dc4
Dmitri Jrapach <dmitrijrapach@gmail.com>
2024-11-28 21:05:35 -0300

    acomodando router

--- Back/src/routes/routes.js
+++ Back/src/routes/routes.js
@@ -1,19 +1,10 @@
 const express = require("express");
-const {
-  register,
-  login,
-  getSpecialty,
-} = require("../controllers/user.controller");
+const { register, login, getSpecialty } = require("../controllers/user.controller");
 const { checkRegister, checkLogin } = require("../middleware/middleware");
-const {
-  getDiagnosis,
-  createDiagnosis,
-} = require("../controllers/doctor.controller");
-const {
-  addHealthcareSystem,
-} = require("../controllers/healthcareSystemController");
+const { getDiagnosis, createDiagnosis } = require("../controllers/doctor.controller");
+const { addHealthcareSystem } = require("../controllers/healthcareSystemController");
 const { reserveTurn } = require("../controllers/turnController.js");
-const AdminController = require('../controllers/adminController');
+const AdminController = require("../controllers/adminController");
 const { getMedicosPorEspecialidad } = require("../controllers/medicoController");
 
 const router = express.Router();
@@ -24,8 +15,8 @@ router.use("/login/api", checkLogin);
 router.post("/register/api", register);
 router.post("/login/api", login);
 router.put("/reserve-turn", reserveTurn);
-router.get('/turnos', AdminController.verTurnos);
-router.get("/appointment",getSpecialty);
+router.get("/turnos", AdminController.verTurnos);
+router.get("/appointment", getSpecialty);
 router.get("/medicos-por-especialidad", getMedicosPorEspecialidad);
 
 router.post("/add", addHealthcareSystem);
