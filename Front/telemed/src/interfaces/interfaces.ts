556b733e52f66cb404fb6902787b7d750b24b639
fhdzleon <fhdzleon@gmail.com>
2024-11-29 00:27:44 -0600

    register form validations and handle erros

--- Front/telemed/src/interfaces/interfaces.ts
+++ Front/telemed/src/interfaces/interfaces.ts
@@ -13,3 +13,17 @@ export interface ModalTurnosProps {
 export interface buttonInformationProps {
   text: string;
 }
+
+export interface registerInputs {
+  name: string;
+  lastName: string;
+  gender: string;
+  phone: string;
+  email: string;
+  password: string;
+  socialWork: string;
+  idSocialWork: string;
+  role: string;
+}
+
+export type registerErrors = Partial<registerInputs>;
