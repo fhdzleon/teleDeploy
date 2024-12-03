c22c21bc3bd8514a31da1ae2928a89d019376bd6
Chech3 <joseangelgarces123@gmail.com>
2024-12-03 14:51:08 -0400

    Agregados cambios en doctorCard y selectSpeciality

--- Front/telemed/src/components/doctor/DoctorCard.tsx
+++ Front/telemed/src/components/doctor/DoctorCard.tsx
@@ -10,15 +10,41 @@ export function DoctorCard({
   especialidad,
   turnosDisponibles,
 }: Medico) {
-  const { selectedValueDate, selectedValueTime, selectedValueDoctor } =
-    useGlobalStore();
+  const {
+    selectedValueDate,
+    selectedValueTime,
+    selectedValueDoctor,
+    setSelectedValueDoctor,
+  } = useGlobalStore();
+
+  const isSelected = selectedValueDoctor === medico;
+
+  if (selectedValueDoctor && !isSelected) {
+    return null;
+  }
 
   const handleSendInformation = () => {
     console.log(selectedValueDate, selectedValueTime, selectedValueDoctor);
   };
 
+  const handleCardClick = () => {
+    if (!isSelected) {
+      setSelectedValueDoctor(medico);
+    }
+  };
+
+  const handleCancel = () => {
+    setSelectedValueDoctor("");
+  };
+  const handleDisabled = !selectedValueDate || !selectedValueTime || !selectedValueDoctor
+
   return (
-    <Card className="w-full max-w-[90vw] md:max-w-6xl flex flex-col md:flex-row">
+    <Card
+      className={`w-full max-w-[90vw] md:max-w-6xl flex flex-col md:flex-row transition-all  ${
+        isSelected ? "" : "cursor-pointer hover:bg-blue-50"
+      } `}
+      onClick={handleCardClick}
+    >
       <CardContent className="p-4 md:p-6 flex flex-col md:flex-row justify-between items-center md:items-start gap-4">
         <div className="space-y-3 w-full md:w-auto">
           <div className="space-y-2 flex flex-col items-center">
@@ -42,13 +68,18 @@ export function DoctorCard({
             </div>
           </div>
         </div>
+
         <TimeSlotPicker medico={medico} turnosDisponibles={turnosDisponibles} />
       </CardContent>
       <div className="flex justify-center md:justify-end items-end p-4 md:p-6 space-x-3 md:space-x-5 transition-all">
-        <Button className="rounded-full bg-red-600 hover:bg-red-700 hover:scale-105">
+        <Button
+          onClick={handleCancel}
+          className="rounded-full bg-red-600 hover:bg-red-700 hover:scale-105"
+        >
           Cancelar
         </Button>
         <Button
+          disabled={handleDisabled}
           onClick={handleSendInformation}
           className="rounded-full bg-green-600 hover:bg-green-700 hover:scale-105"
         >
