00afca0521504dbb7364a1c80563edf4619b5d94
Dmitri Jrapach <dmitrijrapach@gmail.com>
2024-12-03 17:58:43 -0300

    mongo seccion

--- Back/src/app.js
+++ Back/src/app.js
@@ -3,6 +3,7 @@ const corsConfig = require("./config/cors");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const session = require("express-session");
+const MongoStore = require("connect-mongo");
 const middleware = require("./middleware/middleware");
 const { connectToDatabase } = require("./db/database");
 const router = require("./routes/routes");
@@ -15,16 +16,20 @@ connectToDatabase();
 
 // Configuración de sesiones
 app.use(
-    session({
+  session({
       secret: process.env.SESSION_SECRET || "my_secret", // Usa una clave secreta desde .env
       resave: false, // No guardar la sesión si no se ha modificado
       saveUninitialized: false, // No guardar sesiones vacías
+      store: MongoStore.create({
+          mongoUrl: process.env.MONGO_URI, // URL de conexión a tu base de datos MongoDB
+          ttl: 14 * 24 * 60 * 60, // Tiempo de vida de las sesiones en segundos (14 días)
+      }),
       cookie: {
-        secure: process.env.NODE_ENV === "production", // Asegúrate de usar HTTPS en producción
-        maxAge: 1000 * 60 * 60 * 24, // Tiempo de vida de la cookie (1 día en este caso)
+          secure: process.env.NODE_ENV === "production", // Asegúrate de usar HTTPS en producción
+          maxAge: 1000 * 60 * 60 * 24, // Tiempo de vida de la cookie (1 día en este caso)
       },
-    })
-  );
+  })
+);
 
 app.use(corsConfig);
 app.use(bodyParser.json());
