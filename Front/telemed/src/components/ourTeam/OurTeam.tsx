06702f96a4c270548c7c935593c9bd16b3f4c498
Chech3 <joseangelgarces123@gmail.com>
2024-12-11 20:18:35 -0400

    Cambios en ourTeam

--- Front/telemed/src/components/ourTeam/OurTeam.tsx
+++ Front/telemed/src/components/ourTeam/OurTeam.tsx
@@ -3,20 +3,20 @@ import People from "./People";
 
 const OurTeam = () => {
   return (
-    <div className="md:flex justify-center w-full hidden">
-      <div className="bg-primary flex flex-col w-full p-8">
-        <h2 className="text-2xl font-bold text-white pb-8 ">Nuestro Equipo:</h2>
-        <div className="flex space-x-60">
-          <div className="flex flex-col p-4 items-center justify-center">
+    <div className="hidden md:block w-full bg-primary">
+      <div className="max-w-8xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+        <h2 className="text-3xl font-bold text-white mb-8">Nuestro Equipo:</h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+          <div className="flex flex-col items-center">
             <People rol="UX/UI" name="Magalí Silva" name2="Maria Paz" photo="magalis" photo2="maria" />
           </div>
-          <div className="flex flex-col p-4 items-center justify-center">
+          <div className="flex flex-col items-center">
             <People rol="Backend" name="Dmitri Jrapach" name2="Agustin Ibarra" photo="dmitri" photo2="agus" />
           </div>
-          <div className="flex flex-col p-4 items-center justify-center">
+          <div className="flex flex-col items-center">
             <People rol="Frontend" name="Felipe Hernández" name2="José Garces" photo="felipe" photo2="jose" />
           </div>
-          <div className="flex flex-col p-4 items-center justify-center">
+          <div className="flex flex-col items-center">
             <People rol="QA Tester Manual" name="Cynthia Mariel" name2="Erika Escalante" photo="cynthia" photo2="cynthia" />
           </div>
         </div>
